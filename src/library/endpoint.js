01fb29183ec7286d66fa9c47e61d9fc94640568a
Benie Developer <benie@brandit.net>
2024-09-05 11:06:06 +0530

    Changed on gitignore file

--- src/library/endpoint.js
+++ src/library/endpoint.js
@@ -1,13 +1,13 @@
+import _ from "lodash";
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 let endpoints = {
     product: 'api/product/1?with[]=images&with[]=productSizes',
 };
-
 let API_ENDPOINTS = {};
 
-for (const [key, value] of Object.entries(endpoints)) {
-  API_ENDPOINTS[key] = `${API_BASE_URL}${value}`;
-}
-
+_.forEach(
+  endpoints,
+  (value, key) => (API_ENDPOINTS[key] = `${API_BASE_URL}${value}`)
+);
 export const apiEndpoint = API_ENDPOINTS;
